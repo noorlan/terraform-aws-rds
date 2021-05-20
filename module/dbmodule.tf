8b1d12de17298dce3fb1436bf21fbd15258696af
EC2 Default User <ec2-user@ip-172-31-41-163.eu-west-2.compute.internal>
2021-05-20 01:33:38 +0000

    added module

---
+++ module/dbmodule.tf
@@ -0,0 +1,38 @@
+module "wordpress_db" {
+    source = "../"
+
+    region              = "us-east-2"
+    identifier          = "dbname"
+    allocated_storage   = 20
+    storage_type        = "gp2"
+    engine              = "mysql"
+    engine_version      = "5.7"
+    instance_class      = "db.t2.micro"
+    name                = "mydb"
+    username            = "foo"
+    publicly_accessible = true
+
+    subnet_ids = [
+        "subnet-08e95b53a305ceb55", 
+        "subnet-0440aa0610ffc5164", 
+        "subnet-005f6860d4455f090"
+    ]
+
+    allowed_hosts = [
+        "50.194.68.230/32",
+        "127.0.0.1/32"
+    ]
+}
+
+output "region" {
+	value = module.wordpress_db.region
+}
+output "subnet_list" {
+	value = module.wordpress_db.subnet_list
+}
+output "allowed_hosts" {
+	value = module.wordpress_db.allowed_hosts
+}
+output "wordpress_db_NAME" {
+	value = module.wordpress_db.DB_NAME
+}
